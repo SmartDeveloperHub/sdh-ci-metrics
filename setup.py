f5e5f8fd5d311466ef7bf64543bb92e1383d308a
Fernando Serena <fernando.serena@centeropenmiddleware.com>
2016-02-01 18:10:30 +0100

    Fixed config script

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SDH-CI-Metrics",
-    version="0.2.6",
+    version="0.2.7",
     author="Fernando Serena",
     author_email="fernando.serena@centeropenmiddleware.com",
     description="A service that calculates a small set of quantitative metrics on data from Jenkins.",
